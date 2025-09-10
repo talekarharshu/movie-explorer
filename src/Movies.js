224a69dcd9228ae911ec583ac42df732554949b4
Harshada Talekar <149597781+talekarharshu@users.noreply.github.com>
2025-09-10 20:55:03 +0530

    Update Movies.js

--- src/Movies.js
+++ src/Movies.js
@@ -3,4 +3,8 @@ export const movies = [
     { id: 2, title: "Interstellar" },
     { id: 3, title: "The Dark Knight" },
     { id: 4, title: "Tenet" },
+    { id: 5, title: "Mad Max Fury Road" },
+    { id: 6, title: "Blade Runner" },
+    { id: 7, title: "Forrest" },
+    { id: 8, title: "The Hangover" },
   ];
